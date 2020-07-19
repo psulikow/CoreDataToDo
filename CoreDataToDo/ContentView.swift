a60a8593ad420f5e318cada24d6da101308f25b3
Paul Sulikowski <psulikow@gmail.com>
2020-07-19 12:51:18 -0400

    Initial commit

---
+++ CoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+//
+//  Created by Paul Sulikowski on 7/19/20.
+//  Copyright © 2020 Paul Sulikowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
